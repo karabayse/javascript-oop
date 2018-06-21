4a122ac0728eba153764052358c114e7a06f0a8a
Kara Bayse <karabayse@gmail.com>
2018-06-21 13:44:33 -0500

    object literal with es6

--- client.js
+++ client.js
@@ -2,3 +2,22 @@ const s1 = 'Hello';
 console.log(typeof s1);
 
 const s2 = new String('Hello');
+
+console.log(window);
+
+window.alert(1);
+alert(1);
+
+console.log(navigator.appVersion);
+
+// Object Literal
+const book1 = {
+  title: 'Book One',
+  author: 'Jane Doe',
+  year: '2013',
+  getSummary: function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  }
+};
+
+console.log(book1.getSummary());
