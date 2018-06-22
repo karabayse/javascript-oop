f85237586bb78ab226c55c76dff4cf84cc288b11
Kara Bayse <karabayse@gmail.com>
2018-06-22 14:49:38 -0500

    object constructor

--- client.js
+++ client.js
@@ -10,6 +10,7 @@ alert(1);
 
 console.log(navigator.appVersion);
 
+
 // Object Literal
 const book1 = {
   title: 'Book One',
@@ -21,3 +22,24 @@ const book1 = {
 };
 
 console.log(book1.getSummary());
+console.log(Object.values(book1));
+console.log(Object.keys(book1));
+
+
+// ES5 Object Constructor
+function Book(title, author, year) {
+  console.log('Book instantiated...');
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
+}
+
+// Instantiate an object -> runs the Book function
+const book2 = new Book('Book Two', 'John Doe', '2018');
+const book3 = new Book('Book Three', 'Juan Doe', '2018');
+
+console.log(book2.title);
+console.log(book3.getSummary());
